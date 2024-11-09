b4dc8bc025d1e3e0f280b2372bb3cc86b3bd6da6
sairamparvatham <parvathamsairam2000@gmail.com>
2024-11-09 17:45:33 +0530

    apdate

---
+++ jbc_workspace/jdbcapplication/src/main/java/com/edubridge/jdbcapplication/App.java
@@ -0,0 +1,13 @@
+package com.edubridge.jdbcapplication;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
